9d27d32d72cd02143a24019dc54e32483a9bc887
yamazaki-hidekuni <yamazaki.hidekuni@gmail.com>
2025-07-27 07:42:46 +0900

    feat: implement blog post schema and update schema imports in Sanity config

---
+++ schemas/index.js
@@ -0,0 +1,10 @@
+// schemas/index.js
+import post from './post'
+
+// Array of schema types
+export const schemaTypes = [
+  post,
+  // add other types here if needed
+]
+
+export default schemaTypes
