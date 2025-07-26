a57e51cc468a6763a8124601d6e459374155be60
yamazaki-hidekuni <yamazaki.hidekuni@gmail.com>
2025-07-27 08:25:48 +0900

    refactor: move schema files from schemas/ to schema/ directory

--- sanity.config.js
+++ sanity.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import { schemaTypes } from './schemas'
+import schemaTypes from './schema/indes'
 
 export default defineConfig({
   name: 'default',
---
+++ schema/indes.js
@@ -0,0 +1,7 @@
+// schemas/index.js
+import post from './post'
+
+export default [
+  post,
+  // ここに他のスキーマがあれば追加
+]
